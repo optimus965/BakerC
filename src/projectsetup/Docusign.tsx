b1cea1cce4e99fb07aaf183b1bc88499c2276fd6
optimus965 <hk9262809@gmail.com>
2025-02-24 08:04:22 +0530

    message

--- src/projectsetup/Docusign.tsx
+++ src/projectsetup/Docusign.tsx
@@ -1,7 +1,239 @@
 export function Docusign() {
-    return (
-        <div>
-            Docusign
+  return (
+    <div className="box">
+      <div className="SpacingBetween1">
+        <svg
+          fill="none"
+          viewBox="0 0 24 24"
+          height="24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          {" "}
+          <path
+            fill="#D92D27"
+            d="M3 3V21H21V3H3ZM19 19H5V5H19V19ZM11 7H13V9H11V7ZM7 7H9V9H7V7ZM15 7H17V9H15V7Z"
+          ></path>{" "}
+        </svg>
+        <div>Docusign Template</div>
+      </div>
+      <div className="SpacingBetween7">
+        <div className="SpacingBetween6">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15.5 14H14.71L14.43 13.73C15.41 12.59 16 11.11 16 9.5C16 5.91 13.09 3 9.5 3C5.91 3 3 5.91 3 9.5C3 13.09 5.91 16 9.5 16C11.11 16 12.59 15.41 13.73 14.43L14 14.71V15.5L19 20.49L20.49 19L15.5 14ZM9.5 14C7.01 14 5 11.99 5 9.5C5 7.01 7.01 5 9.5 5C11.99 5 14 7.01 14 9.5C14 11.99 11.99 14 9.5 14Z"
+              fill="#9B9B9B"
+            ></path>
+          </svg>
+          <div className="noInput">
+            <input type="text" />
+          </div>
         </div>
-    )
+        <div className="SpacingBetween8">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z"
+              fill="#D92D27"
+            ></path>
+          </svg>
+          <div>Add Template</div>
+        </div>
+      </div>
+      <div className="ParentDiv">
+        <div className="TableDiv">
+          <table>
+            <thead>
+              <tr>
+                <td>No.</td>
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+                <th className="tableData">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>12 Documents</td>
+                <td>12</td>
+                <td></td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>2</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>3</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>4</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>5</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>6</td>
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>7</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>8</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>9</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>10</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>11</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>12</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>13</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>14</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>15</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>16</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>17</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+              <tr>
+                <td>18</td>
+
+                <td>Template Name</td>
+                <td>Count</td>
+                <td>Template Category</td>
+
+                <td className="tableData">Edit </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        {/* <div>
+        <table>
+            <thead>
+              <tr>Actions</tr>
+            </thead>
+          </table>
+        </div> */}
+      </div>
+    </div>
+  );
 }
