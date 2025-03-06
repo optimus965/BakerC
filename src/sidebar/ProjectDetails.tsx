4d2d140a0abbc8213b6d670e579c0c9042b0c267
optimus965 <hk9262809@gmail.com>
2025-03-06 08:38:36 +0530

    commit message

--- src/sidebar/ProjectDetails.tsx
+++ src/sidebar/ProjectDetails.tsx
@@ -1,47 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { titles } from "../data/title";
 import { IconButton } from "../components/IconButton";
+import { Header } from "../projectsetup/Header";
 export function ProjectDetails() {
   return (
     <div className="heightFixed">
-      <div className="ProjectSetUp">
-        <div style={{fontSize:"20px"}}>Project Setup</div>
-        <div className="IconJustify">
-          {" "}
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 16 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="Notifications">
-                <path
-                  id="Vector"
-                  d="M8.00033 14.667C8.73366 14.667 9.33366 14.067 9.33366 13.3337H6.66699C6.66699 14.067 7.26033 14.667 8.00033 14.667ZM12.0003 10.667L12.0003 7.33366C12.0003 5.28699 10.907 3.57366 9.00033 3.12033V2.66699C9.00033 2.11366 8.55366 1.66699 8.00033 1.66699C7.44699 1.66699 7.00033 2.11366 7.00033 2.66699V3.12033C5.08699 3.57366 4.00033 5.28033 4.00033 7.33366V10.667L2.66699 12.0003V12.667L13.3337 12.667V12.0003L12.0003 10.667Z"
-                  fill="#3A3737"
-                ></path>
-              </g>
-            </svg>
-            <div className="Profile">U</div>
-          <div className="Icon">
-            <svg
-              width="20"
-              height="20"
-              viewBox="0 0 16 17"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M4.66666 11.8333L5.60666 10.8933L3.88666 9.16667L10.6667 9.16667L10.6667 7.83333L3.88666 7.83333L5.60666 6.11333L4.66666 5.16667L1.33332 8.5L4.66666 11.8333ZM13.3333 13.1667L7.99999 13.1667L7.99999 14.5L13.3333 14.5C14.0667 14.5 14.6667 13.9 14.6667 13.1667L14.6667 3.83333C14.6667 3.1 14.0667 2.5 13.3333 2.5L7.99999 2.5L7.99999 3.83333L13.3333 3.83333L13.3333 13.1667Z"
-                fill="#303030"
-                fill-opacity="0.5"
-              ></path>
-            </svg>
-            <div>Logout</div>
-          </div>
-        </div>
-      </div>
+      <Header title='project Details' svgElement={''}/>
       <div className="Responsive-TabBar">
         {titles.map((item, index) => {
           return <IconButton key={item} index={index} />;
--- src/sidebar/UnitListView.tsx
+++ src/sidebar/UnitListView.tsx
@@ -1,5 +1,420 @@
+import React, { JSX, useState } from "react";
+import { Header } from "../projectsetup/Header";
+enum unitType {
+  BedRoom = "BedRoom",
+  Studio = "Studio",
+  Other = "Other",
+}
+enum Standard {
+  Luxury = "Luxury",
+  Deluxe = "Deluxe",
+}
+interface Data {
+  UnitType: unitType;
+  Floor: number;
+  Model: Standard;
+  // SquareFeet: number;
+  LaunchPrice: number;
+  // BedRooms: number;
+  // BathRoom: number;
+  // MaximumLockerCountNumber: number;
+  // MaximumParkingCountNumber: number;
+  // UnitPricePerSqFeet: number;
+  BuildingName: string;
+  unitName: string;
+  status: string;
+  AllocatedStatus: string;
+  CoOpAgent: string;
+  level2Status: string;
+  tags?: string[];
+}
 export function UnitListView() {
-    return (
-        <div>UnitListView</div>
-    )
+  const [data, setData] = useState<Data[]>([]);
+  const [otherTable, setOtherTable] = useState<boolean>(true);
+  const [otherTable1, setOtherTable1] = useState<boolean>(false);
+  const [searchTerm,setSearchTerm] = useState<string>(''); 
+  const filteredData = data.filter(item =>
+    searchTerm === "" || item.status.toLowerCase() === searchTerm.toLowerCase())
+    console.log(searchTerm)
+    console.log(filteredData)
+  const [current, setCurrent] = useState<Data>({
+    UnitType: unitType.Other,
+    Floor: 0,
+    Model: Standard.Deluxe,
+    // SquareFeet: 0,
+    LaunchPrice: 0,
+    // IncreaseInRevenueSinceLaunch: 0,
+    // BedRooms: 0,
+    // BathRoom: 0,
+    // MaximumLockerCountNumber: 0,
+    // MaximumParkingCountNumber: 0,
+    // UnitPricePerSqFeet: 0,
+    BuildingName: "Antilia",
+    unitName: "",
+    status: "",
+    level2Status: "",
+    AllocatedStatus: "",
+    CoOpAgent: "",
+  });
+  const [visible, setVisible] = useState<Boolean>(false);
+  function handleClick() {
+    if (
+      current.unitName.length > 0 &&
+      current.status.length > 0 &&
+      current.level2Status.length > 0
+    ) {
+      setData((prev) => {
+        prev.push(current);
+        return data;
+      });
+      setCurrent((prev) => {
+        const data: Data = {
+          UnitType: unitType.Other,
+          Floor: 0,
+          Model: Standard.Deluxe,
+          // SquareFeet: 0,
+          LaunchPrice: 0,
+          // IncreaseInRevenueSinceLaunch: 0,
+          // BedRooms: 0,
+          // BathRoom: 0,
+          // MaximumLockerCountNumber: 0,
+          // MaximumParkingCountNumber: 0,
+          // UnitPricePerSqFeet: 0,
+          BuildingName: "Antilia",
+          unitName: "",
+          status: "",
+          level2Status: "",
+          AllocatedStatus: "",
+          CoOpAgent: "",
+        };
+        return data;
+      });
+      setVisible(!visible);
+    }
+  }
+  function handleChange(
+    event: React.ChangeEvent<HTMLSelectElement>,
+    unitName: string
+  ) {
+    console.log("hello, world");
+    const data1 = data.map((item) => {
+      if (item.unitName === unitName) {
+        return {
+          ...item,
+          tags: [...(item.tags || []), event.target.value],
+        };
+      }
+      return item;
+    });
+    console.log(data1);
+    setData(data1);
+  }
+  return (
+    <div>
+      <Header
+        title={"Units List View"}
+        svgElement={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            height="24"
+            width="24"
+          >
+            {" "}
+            <path
+              fill="#DA1E28"
+              d="M18.4 4H5.6C4.8 4 4 4.72 4 5.6V8.008C4 8.584 4.344 9.08 4.8 9.36V18.4C4.8 19.28 5.68 20 6.4 20H17.6C18.32 20 19.2 19.28 19.2 18.4V9.36C19.656 9.08 20 8.584 20 8.008V5.6C20 4.72 19.2 4 18.4 4ZM14.4 13.6H9.6V12H14.4V13.6ZM18.4 8H5.6V5.6L18.4 5.584V8Z"
+            ></path>{" "}
+          </svg>
+        }
+      />
+      <div className="SearchTerm"><label htmlFor="delivery" className="control-label ">Search</label><div> <input value={searchTerm} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setSearchTerm(e.target.value)} type="text" /></div></div>
+      <div className="SpacingBetween7">
+        <div className="SpacingBetween11">
+          <div className="selectBox">
+            {" "}
+            <div>
+              <input
+                checked={otherTable}
+                type="checkbox"
+                onChange={() => setOtherTable(!otherTable)}
+              />
+            </div>
+            <label htmlFor="delivery" className="control-label ">
+              Unit Attributes
+            </label>
+          </div>
+          <div className="selectBox">
+            {" "}
+            <div>
+              <input checked={otherTable1} onChange={()=>setOtherTable1(!otherTable1)} type="checkbox" />
+            </div>
+            <label htmlFor="delivery" className="control-label ">
+              Allocations
+            </label>
+          </div>
+          <div className="selectBox">
+            {" "}
+            <div>
+              <input type="checkbox" />
+            </div>
+            <label htmlFor="delivery" className="control-label ">
+              Co-op
+            </label>
+          </div>
+          <div className="selectBox">
+            {" "}
+            <div>
+              <input type="checkbox" />
+            </div>
+            <label htmlFor="delivery" className="control-label ">
+              Purchasers
+            </label>
+          </div>
+          <div className="selectBox">
+            {" "}
+            <div>
+              <input type="checkbox" />
+            </div>
+            <label htmlFor="delivery" className="control-label ">
+              Transactions
+            </label>
+          </div>
+          <div className="selectBox">
+            {" "}
+            <div>
+              <input type="checkbox" />
+            </div>
+            <label htmlFor="delivery" className="control-label ">
+              Deposits
+            </label>
+          </div>
+        </div>
+        <div className="SpacingBetween1"><div className="SpacingBetween8" onClick={() => setVisible(!visible)}>
+          Add New
+        </div>
+       
+        <div className="SpacingBetween8">
+          <svg
+            fill="none"
+            viewBox="0 0 24 24"
+            height="24"
+            width="24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill="#DA291C"
+              d="M5 20H19V18H5V20ZM19 9H15V3H9V9H5L12 16L19 9Z"
+            ></path>
+          </svg>
+          Download Units List
+        </div></div>
+ {visible && (
+          <div className="Form3">
+            <div>
+              <label>UnitType</label>
+              <select
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  setCurrent((prev) => ({
+                    ...prev,
+                    UnitType: e.target.value as unitType,
+                  }))
+                }
+              >
+                <option value="BedRoom">BedRoom</option>
+                <option value="Studio">Studio</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+            <div>
+              {" "}
+              <label>Status</label>
+              <input
+                value={current?.status}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCurrent((prev) => ({
+                    ...prev,
+                    status: e.target.value,
+                  }))
+                }
+              />
+            </div>
+            <div>
+              <label>Level 2 Status</label>
+              <input
+                value={current?.level2Status}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCurrent((prev) => ({
+                    ...prev,
+                    level2Status: e.target.value,
+                  }))
+                }
+              />
+            </div>
+            <div>
+              <label>Allocated Status</label>
+              <input
+                value={current?.AllocatedStatus}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCurrent((prev) => ({
+                    ...prev,
+                    AllocatedStatus: e.target.value,
+                  }))
+                }
+              />
+            </div>
+            <div>
+              <label>CoOpAgent</label>
+              <input
+                value={current?.CoOpAgent}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCurrent((prev) => ({
+                    ...prev,
+                    CoOpAgent: e.target.value,
+                  }))
+                }
+              />
+            </div>
+            <div>
+              <label>Floor</label>
+              <input
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                  setCurrent((prev) => ({
+                    ...prev,
+                    Floor: parseInt(event.target.value),
+                  }))
+                }
+              />
+            </div>
+            <div>
+              <label>Model</label>
+              <select
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  setCurrent((prev) => ({
+                    ...prev,
+                    Model: e.target.value as Standard,
+                  }))
+                }
+              >
+                <option value="Luxury">Luxury</option>
+                <option value="Deluxe">Deluxe</option>
+              </select>
+            </div>
+            <div>
+              <label>Launch Price</label>
+              <input
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCurrent((prev) => ({
+                    ...prev,
+                    LaunchPrice: parseInt(e.target.value),
+                  }))
+                }
+              />
+            </div>
+            <div>
+              <label>Building Name</label>
+              <input
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCurrent((prev) => ({
+                    ...prev,
+                    BuildingName: e.target.value,
+                  }))
+                }
+              />
+            </div>
+            <div>
+              <label>Unit Name</label>
+              <input
+                value={current?.unitName}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setCurrent((prev) => ({
+                    ...prev,
+                    unitName: e.target.value,
+                  }))
+                }
+              />
+            </div>
+            <div>
+              <button onClick={handleClick}>Save</button>
+            </div>
+          </div>
+        )}
+
+
+      </div>
+      <div className="SecondTable">
+        <table>
+          <thead>
+            <tr>
+              <td>Unit Name</td>
+              <td>Status</td>
+              {otherTable && (
+                <>
+                  <td>Floor</td>
+                  <td>Model</td>
+                  <td>Launch price</td>
+                  <td>Building Name</td>
+                </>
+              )}
+              {otherTable1 && (
+                <>
+                  <td>Allocated Status</td>
+                  <td>Co-Op Agent</td>
+                </>
+              )}
+              <td>Level 2 Status</td>
+              <td>Tags</td>
+              <td>Actions</td>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredData.map((item) => {
+              return (
+                <tr key={item.unitName}>
+                  <td>{item.unitName}</td>
+                  <td>{item.status}</td>
+                  {otherTable && (
+                    <>
+                      <td>{item.Floor}</td>
+                      <td>{item.Model}</td>
+                      <td>{item.LaunchPrice}</td>
+                      <td>{item.BuildingName}</td>
+                    </>
+                  )}
+                  {otherTable1 && (
+                    <>
+                      <td>{item.AllocatedStatus}</td>
+                      <td>{item.CoOpAgent}</td>
+                    </>
+                  )}
+                  <td>{item.level2Status}</td>
+                  <td>
+                    <div className="box1">
+                      <div className="SpacingBetween12">
+                        {item.tags &&
+                          item?.tags.map((item) => {
+                            return <div>{item}</div>;
+                          })}
+                      </div>
+                      <select
+                        onChange={(
+                          event: React.ChangeEvent<HTMLSelectElement>
+                        ) => handleChange(event, item.unitName)}
+                      >
+                        <option>hello world</option>
+                        <option>helo world1</option>
+                        <option>aaaaaaaaaaaaaaaaaaaaa</option>
+                      </select>
+                    </div>
+                  </td>
+                  <td></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
